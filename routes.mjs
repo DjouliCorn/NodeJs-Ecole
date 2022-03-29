8fd6071b8e0194a5b2a456905a0b7a725ee462fd
Michel <michel@MacBook-Pro-de-Michel.local>
2022-03-29 17:17:36 +0200

    routeur en cours

--- routes.mjs
+++ routes.mjs
@@ -1,6 +1,7 @@
+import express from "express";
 import { add, getAll, getByName, update, remove } from "./products.mjs";
 
-const route = async (request, response) => {
+/*const route = async (request, response) => {
     if(request.method === "POST") {
         const body = await postMethod(request);
         add(body.name, body.quantity) 
@@ -28,6 +29,56 @@ const postMethod = (request) => {
             return resolve(JSON.parse(body));
         });
     });
+};*/
+
+const router = express.Router()
+
+
+router.get('/about', function(req, res) {
+    console.log(req)
+    res.send('About birds');
+  });
+
+  router.post('/about', function(req, res) {
+    add()
+    res.send('About birds');
+  });
+  router.get('/about:name', function(req, res) {
+      const name = req.params.name
+      const result = JSON.stringify(getByName(name));
+    res.send(result);
+  });
+
+
+
+
+
+router.get('/products', function (req, res) {
+    let getUrl = req.url.split("/")
+        if(getUrl[1] === "products") {
+            if(getUrl[2]){
+                req.body = JSON.stringify(getByName(getUrl[2]));
+            } else {
+                req.body = JSON.stringify(getAll());
+            }
+        }
+    res.send(req.body)
+});
+
+
+const postMethod = (request) => {
+    let body = ""
+
+    return new Promise((resolve, reject) => {
+        request.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        request.on("end", () => {
+            return resolve(JSON.parse(body));
+        });
+    });
 };
 
-export default route;
+module.exports = router;
+
+//export default route;
