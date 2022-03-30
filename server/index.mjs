b4e226677da433435908472f7c00fac310c24409
Michel <michel@MacBook-Pro-de-Michel.local>
2022-03-30 17:04:02 +0200

    ajout client

---
+++ server/index.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import router from "./routes.mjs";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+const port = process.env.PORT || 3000;
+const host = "127.0.0.1";
+
+var app = express()
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+app.use(router)
+
+app.listen(port, "0.0.0.0");
+console.log("\x1b[36m%s\x1b[0m", `\nListening at http://${host}:${port}`);
+
+
