45f4b726ae0a2e7bf9dfb595bde59ce31b25f47d
Michel <michel@MacBook-Pro-de-Michel.local>
2022-03-30 11:15:06 +0200

    express indent

--- index.mjs
+++ index.mjs
@@ -1,21 +1,12 @@
 import express from "express";
 import router from "./routes.mjs"
 
-/*const server = http.createServer(async (request, response) => {
-    let body = ""
-
-    const res = await route(request, body);
-
-    response.write(res)
-    response.end();
-})*/
-
 const port = 3000;
 const host = "127.0.0.1";
 
 var app = express()
-    app.use(express.json({extended: false}));
-    app.use(router)
+app.use(express.json({ extended: false }));
+app.use(router)
 
 app.listen(port, host);
 console.log("\x1b[36m%s\x1b[0m", `\nListening at http://${host}:${port}`);
--- routes.mjs
+++ routes.mjs
@@ -1,40 +1,10 @@
 import express from "express";
 import { add, getAll, getByName, update, remove } from "./products.mjs";
 
-/*const route = async (request, response) => {
-    if(request.method === "POST") {
-        const body = await postMethod(request);
-        add(body.name, body.quantity) 
-        return "produit ajouté"
-    } else {
-        let getUrl = request.url.split("/")
-        if(getUrl[1] === "products") {
-            if(getUrl[2]){
-                return JSON.stringify(getByName(getUrl[2]));
-            } else {
-                return JSON.stringify(getAll());
-            }
-        }
-    }
-}
-
-const postMethod = (request) => {
-    let body = ""
-
-    return new Promise((resolve, reject) => {
-        request.on("data", (chunk) => {
-            body += chunk.toString();
-        });
-        request.on("end", () => {
-            return resolve(JSON.parse(body));
-        });
-    });
-};*/
-
 const router = express.Router()
 
 router.post('/products', function (req, res) {
-    const {name, quantity} = req.body
+    const { name, quantity } = req.body
     res.send(add(name, quantity));
 });
 
