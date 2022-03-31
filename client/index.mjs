81b0168fd68a1027c916a640826b23fcdbd861a2
Michel <michel@MacBook-Pro-de-Michel.local>
2022-03-31 11:50:30 +0200

    add product

--- client/index.mjs
+++ client/index.mjs
@@ -1,15 +1,29 @@
 import axios from "axios"
 
+let url = 'http://localhost:3000/products'
 
 function getProducts() {
-    axios.get('http://localhost:3000/products').then(function (response) {
+    axios.get(url).then(function (response) {
         console.log(response.data)
     }).catch(function (error) {
         console.error(error)
     })
 }
 
+function addProduct() {
+    axios.post(url, {
+        name: "pêche",
+        quantity: 12
+    }).then(function (response) {
+        console.log(response.data)
+    }).catch(function (error) {
+        console.error(error)
+    })
+}
+
+addProduct()
 getProducts()
+
 
 
 //IIFE
