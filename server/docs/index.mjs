f8e869c8de015f65a96aea16cea5cd5373944392
Michel <michel@MacBook-Pro-de-Michel.local>
2022-04-01 12:09:59 +0200

    swagger installed

---
+++ server/docs/index.mjs
@@ -0,0 +1,13 @@
+export default {
+    openapi: "3.0.1",
+    info: {
+        version: "1.0.0",
+        title: "ESGI API",
+        description: "ESGI API Rest",
+        constact: {
+            name: "ESGI Contacts",
+            email: "info@esgi.fr",
+            url: "https://esgi.fr",
+        },
+    },
+};
