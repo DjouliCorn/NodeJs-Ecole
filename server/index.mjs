7ca38e85e308c7d610767589519f859fc46f14c8
Michel <michel@MacBook-Pro-de-Michel.local>
2022-04-01 16:20:10 +0200

    changing the port with dotenv

--- server/index.mjs
+++ server/index.mjs
@@ -4,7 +4,10 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import swaggerUI from 'swagger-ui-express'
 import docs from './docs/index.mjs'
+import dotenv from 'dotenv'
 
+//change the port by the one which is written in the .env 
+dotenv.config()
 var app = express()
 /* eslint-disable no-undef*/
 const port = process.env.PORT || 3000
@@ -15,5 +18,8 @@ app.use(morgan('tiny'))
 app.use(router)
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(docs))
 
+//'0.0.0.0' : changing for Heroku
 app.listen(port, '0.0.0.0')
 console.log('\x1b[36m%s\x1b[0m', `\nListening at http://${host}:${port}`)
+//console.log(process.env)
+
